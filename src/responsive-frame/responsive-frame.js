2d02028e7720a186f3dbd0248c73a8d4c8adcf72
jhartST <jhartST@users.noreply.github.com>
2019-08-20 15:11:58 -0700

    Updating to use V1 of the custom elements API (#3)
    * Updating to use V1 of the custom elements API
    
    Also includes polyfills for Reflect method and custom elements V0 API, needed for Edge < 18 and IE11. Tried not to break anything.
    
    * It belongs to us all now

--- src/responsive-frame/responsive-frame.js
+++ src/responsive-frame/responsive-frame.js
@@ -1,73 +1,99 @@
+require("core-js/es/reflect");
+require('@webcomponents/custom-elements');
 require("./responsive-frame.less");
 var Host = require("./host");
-require("document-register-element");
 var makeEvent = require("../makeEvent");
 
-var proto = Object.create(HTMLElement.prototype);
-var iframeProto = Object.create(HTMLIFrameElement.prototype);
-proto.attachedCallback = iframeProto.attachedCallback = function() {
-  this.upgrade();
+
+function ResponsiveFrame() {
+    return Reflect.construct(HTMLElement, [], this.constructor);
 }
 
-proto.upgrade = iframeProto.upgrade = function() {
-  if (this.upgraded_) return;
-  this.upgraded_ = true;
-  var src = this.getAttribute("src");
-  var element;
-  if (this.tagName.toLowerCase() == "iframe") {
-    element = this;
-  } else {
-    var root = this.attachShadow ? this.attachShadow({ mode: "open" }) : this;
-    element = document.createElement("iframe");
-    element.src = src;
-    root.appendChild(element);
-  }
-  element.setAttribute("seamless", "");
-  element.setAttribute("width", "100%");
-  element.setAttribute("scrolling", "no");
-  element.setAttribute("horizontalscrolling", "no");
-  element.setAttribute("verticalscrolling", "no");
-  element.setAttribute("frameborder", "0");
-  element.setAttribute("marginwidth", "0");
-  element.setAttribute("marginheight", "0");
-  element.setAttribute("mozallowfullscreen", "");
-  element.setAttribute("webkitallowfullscreen", "");
-  element.setAttribute("allowfullscreen", "");
-  element.style.display = "block";
-  this.listen(element);
+ResponsiveFrame.prototype = Object.create(HTMLElement.prototype);
+ResponsiveFrame.prototype.constructor = ResponsiveFrame;
+Object.setPrototypeOf(ResponsiveFrame, HTMLElement);
+
+ResponsiveFrame.prototype.connectedCallback = function() {
+    this.upgrade();
 };
 
-proto.listen = iframeProto.listen = function(frame) {
-  if (this.host) this.host.destroy();
-  var self = this;
-  this.host = new Host(frame, function(data) {
-    self.dispatchEvent(makeEvent("childmessage", { data: data, bubbles: true }));
-  });
-};
-
-proto.attributeChangedCallback = iframeProto.attributeChangedCallback = function(attr, oldVal, newVal) {
-  if (attr != "src") return;
-  var frame = this; // start assuming iframe is="responsive-iframe"
-  if (frame.tagName.toLowerCase() != "iframe") {
-    var root = this.shadowRoot ? this.shadowRoot : this;
-    frame = root.querySelector("iframe");
-    if (frame.getAttribute("src") == newVal) return;
-    frame.src = newVal;
-  }
-  //wait for the frame to start transitioning
-  var self = this;
-  setTimeout(function() {
-    self.listen(frame)
-  }, 100);
+function ResponsiveIFrame() {
+    return Reflect.construct(HTMLIFrameElement, [], this.constructor);
 }
 
-proto.sendMessage = iframeProto.sendMessage = function(message) {
-  this.host.send(message);
+ResponsiveIFrame.prototype = Object.create(HTMLIFrameElement.prototype);
+ResponsiveIFrame.prototype.constructor = ResponsiveIFrame;
+Object.setPrototypeOf(ResponsiveIFrame, HTMLIFrameElement);
+
+ResponsiveIFrame.prototype.connectedCallback = function() {
+    this.upgrade();
+};
+
+
+ResponsiveFrame.prototype.upgrade = ResponsiveIFrame.prototype.upgrade = function() {
+    if (this.upgraded_) return;
+    this.upgraded_ = true;
+    var src = this.getAttribute("src");
+    var element;
+    if (this.tagName.toLowerCase() == "iframe") {
+        element = this;
+    } else {
+        var root = this.attachShadow ? this.attachShadow({
+            mode: "open"
+        }) : this;
+        element = document.createElement("iframe");
+        element.src = src;
+        root.appendChild(element);
+    }
+    element.setAttribute("seamless", "");
+    element.setAttribute("width", "100%");
+    element.setAttribute("scrolling", "no");
+    element.setAttribute("horizontalscrolling", "no");
+    element.setAttribute("verticalscrolling", "no");
+    element.setAttribute("frameborder", "0");
+    element.setAttribute("marginwidth", "0");
+    element.setAttribute("marginheight", "0");
+    element.setAttribute("mozallowfullscreen", "");
+    element.setAttribute("webkitallowfullscreen", "");
+    element.setAttribute("allowfullscreen", "");
+    element.style.display = "block";
+    this.listen(element);
+};
+
+ResponsiveFrame.prototype.listen = ResponsiveIFrame.prototype.listen = function(frame) {
+    if (this.host) this.host.destroy();
+    var self = this;
+    this.host = new Host(frame, function(data) {
+        self.dispatchEvent(makeEvent("childmessage", {
+            data: data,
+            bubbles: true
+        }));
+    });
+};
+
+ResponsiveFrame.prototype.attributeChangedCallback = ResponsiveIFrame.prototype.attributeChangedCallback = function(attr, oldVal, newVal) {
+    if (attr != "src") return;
+    var frame = this; // start assuming iframe is="responsive-iframe"
+    if (frame.tagName.toLowerCase() != "iframe") {
+        var root = this.shadowRoot ? this.shadowRoot : this;
+        frame = root.querySelector("iframe");
+        if (frame.getAttribute("src") == newVal) return;
+        frame.src = newVal;
+    }
+    //wait for the frame to start transitioning
+    var self = this;
+    setTimeout(function() {
+        self.listen(frame)
+    }, 100);
+}
+
+ResponsiveFrame.prototype.sendMessage = ResponsiveIFrame.prototype.sendMessage = function(message) {
+    this.host.send(message);
 };
 
 try {
-  document.registerElement("responsive-frame", { prototype: proto });
-  document.registerElement("responsive-iframe", { prototype: iframeProto, extends: "iframe" });
+    customElements.define('responsive-frame', ResponsiveFrame);
+    customElements.define('responsive-iframe', ResponsiveIFrame);
 } catch (_) {
-  if (window.console && console.log) console.log("<responsive-iframe> is already registered");
+    if (window.console && console.log) console.log("<responsive-iframe> is already registered");
 }
